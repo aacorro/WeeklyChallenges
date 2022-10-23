33002ae97dbb91e982927cadab42c09c915c9a37
Abdiel Corro <acorrop@outlook.com>
2022-10-22 23:23:38 -0500

    challenge four completed

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,75 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(k => k % 2 != 0).Sum();
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            string[] words = { str1, str2, str3, str4 };
+            return words.Min(p => p.Length);
+
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] num = { number1, number2, number3, number4 };
+            return num.Min(p => p);
+
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return (
+
+                    sideLength1 + sideLength2 > sideLength3 &&
+                    sideLength1 + sideLength3 > sideLength2 &&
+                    sideLength3 + sideLength2 > sideLength1
+                  );
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            try
+            {
+                double.Parse(input);
+                return true;
+            } catch { return false; }
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
-        {
-            throw new NotImplementedException();
-        }
+           => objs.Where((o) => o == null).Count() > objs.Where((o) => o != null).Count() ?
+            true : false;
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
-        }
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            var x = numbers.Where(x => x % 2 == 0);
+            if (x.Count() == 0)
+            {
+                return 0;
+            }
+            return x.Average();
 
+        }
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            int fact = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                 fact = fact * i;
+            }
+            return fact;
         }
+        
     }
 }
